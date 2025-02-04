e030b0c004e15905028f875319f17049c87563e5
Josh Laidlaw <josh.laidlaw@gmail.com>
2025-02-04 11:53:15 -0800

    Rename TicketView to Tickets

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import ButtonCart from './components/ButtonCart.vue';
 <template>
   <nav class="flex flex-row gap-4 items-center p-7 bg-gray-100 border-b-2 border-gray-200">
     <RouterLink class="hover:underline" to="/admin">Admin</RouterLink>
-    <RouterLink class="hover:underline" to="/">TicketView</RouterLink>
+    <RouterLink class="hover:underline" to="/">Tickets</RouterLink>
     <ButtonCart />
   </nav>
   <main class="mx-auto p-4">
